60b7dc58708f4feae1164971f73e28d94513b8f1
yourname <1192762465@qq.com>
2023-12-06 17:11:26 +0800

    leetcode69 update

--- array/bs.cpp
+++ array/bs.cpp
@@ -126,6 +126,49 @@ public:
         }
 
     }
+
+    int mySqrt(int x) 
+    {
+        int n = 0;
+        if(x == 0)
+        {
+            return 0;
+        }
+        while (n*n<=x)
+        {
+            if(n*n<=x && (long long)(n+1)*(n+1)>x)
+            {
+                return n;
+            }
+            n++;
+        }
+        return n;
+        
+    }
+
+    int mySqrt_change(int x) 
+    {
+        int left , right , middle;
+        left = 0;
+        right = x;
+        int res = 0;
+        while (left<=right)
+        {
+            middle = left + (right-left)/2;
+            if((long long)middle*middle<=x)
+            {
+                res = middle;
+                left = middle + 1;
+            }
+            else
+            {
+                right = middle - 1;
+            }
+        }
+        return res;
+        
+        
+    }
 private:
     int getright(vector<int>& nums, int target)
     {
@@ -174,21 +217,27 @@ private:
 
 int main(int argc,char **argv)
 {
-    /* 704 */
     Solution solution;
+    /* 704 */
+    /* Solution solution;
     vector<int> test = {0,1,2,6,7,11,66,89,94,102};
     int index = solution.search2(test,102);
-    cout<<index<<endl;
+    cout<<index<<endl; */
     /* 35 */
-    int index2 = solution.searchInsert2(test,-2);
-    cout<<index2<<endl;
+    /* int index2 = solution.searchInsert2(test,-2);
+    cout<<index2<<endl; */
     /* 34 */
-    vector<int> test2 = {5,7,7,8,8,10};
+    /* vector<int> test2 = {5,7,7,8,8,10};
     vector<int> res = solution.searchRange(test2,8);
     for(auto &a : res)
     {
         cout<<a;
-    }
+    } */
+    /* 69 */
+    int n = 4;
+    int res = solution.mySqrt(n);
+    int res2 = solution.mySqrt_change(n);
+    cout<<res<<res2<<endl;
 
     return 0;
 }
